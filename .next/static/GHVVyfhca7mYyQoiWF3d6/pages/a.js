2aa374f088d8228956df6f40f7cd630f7a8064de
nmakuch <makuch.nick@gmail.com>
2019-11-03 12:10:13 -0500

    init

---
+++ .next/static/GHVVyfhca7mYyQoiWF3d6/pages/a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{W4Pv:function(n,o,t){"use strict";t.r(o);var u=t("q1tI"),c=t.n(u).a.createElement;o.default=function(){return c("div",null,"a")}},cNoJ:function(n,o,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/a",function(){return t("W4Pv")}])}},[["cNoJ",1,0]]]);
